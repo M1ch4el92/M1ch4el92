ccfb485e5781b44996303cb322dcb5a0af2b24ef
M1ch4el92 <81914484+M1ch4el92@users.noreply.github.com>
2025-01-03 15:03:27 +0300

    Update password_generator.py

--- my_projects/password_generator.py
+++ my_projects/password_generator.py
@@ -10,16 +10,16 @@ numbers = '1234567890'
 symbols = '~!@#$%^&*()_+=-№\/'
 
 for l in range(int(input("Welcome to password generator!\nHow much letters you want in your password?\n"))):
-    my_password = my_password + random.choice(letter)
+    my_password += random.choice(letter)
 
 for cl in range(int(input("How much capital letters you want in your password?\n"))):
-    my_password = my_password + random.choice(cap_letter)
+    my_password += random.choice(cap_letter)
 
 for n in range(int(input("How much numbers you want in your password?\n"))):
-    my_password = my_password + random.choice(numbers)
+    my_password += random.choice(numbers)
    
 for s in range(int(input("How much symbols you want in your password?\n"))):
-    my_password = my_password + random.choice(symbols)
+    my_password += random.choice(symbols)
 
 my_password = list(my_password) #преобразуем строку в список
 
